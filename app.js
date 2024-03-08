1cfa0b20c3f50317a50c50d45cff6a4db26518ad
claudio henriquez <cclaudiohenriquez@gmail.com>
2024-03-08 14:13:06 -0300

    dia1.1

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const saludo = require('./saludo.js');
-console.log(saludo.saludar);
-console.log(saludo.saludarHolaMundo('jose'));
+//const saludo = require('./saludo.js');
+//console.log(saludo.saludar);
+//console.log(saludo.saludarHolaMundo('jose'));
+
+
+
+const {saludarHolaMundo} = require('./saludo.js')
+console.log(saludarHolaMundo('jose')+" g");
+
+
+
+
